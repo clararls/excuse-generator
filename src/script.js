897dd488a874aef87398d576df5dce665a045692
Clara Rojano León-Salas <clararojanols@gmail.com>
2021-09-11 16:41:07 +0000

    2

---
+++ src/script.js
@@ -0,0 +1,20 @@
+/* eslint-disable */
+import "bootstrap";
+import "./style.css";
+
+window.onload = function() {
+  //write your code here
+  var who = ["the dog", "my father", "my girlfriend", "my cat"];
+  var what = ["scratched", "took away", "stole", "broked"];
+  var when = [
+    "before the class",
+    "during dinner",
+    "when I was in the shower",
+    "taking the bus"
+  ];
+  var ex1 = Math.floor((Math.random()*who.length) );
+  var ex2 = Math.floor((Math.random()*what.length));
+  var ex2 = Math.floor((Math.random()*when.length));
+  
+  document.querySelector ("#excuse").innerHTML = who[ex1]+what[ex2]+when[ex3];
+};
